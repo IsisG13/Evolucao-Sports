4b10d0352119323c6c8b4daf4ec3dddf7ea0ad39
Isis <isissouzaguimaraes@gmail.com>
2023-10-17 19:19:37 -0300

    sem componentes

--- src/menu/Details.js
+++ src/menu/Details.js
@@ -20,8 +20,6 @@ import "../css/magnific-popup.css";
 import "../css/slicknav.min.css";
 import "../css/style.css";
 import { Link } from "react-router-dom";
-import Cabecalho from "../components/cabecalho";
-import Footer from "../components/footer";
 
 function Details() {
   useEffect(() => {
@@ -37,7 +35,124 @@ function Details() {
   }, []);
   return (
     <div className="App">
-      <Cabecalho />
+      {/* Page Preloder */}
+      <div className="preloder">
+        <div className="loader"></div>
+      </div>
+
+      {/* Offcanvas Menu Section Begin */}
+      <div className="offcanvas-menu-overlay active"></div>
+      <div className="offcanvas-menu-wrapper">
+        <div className="canvas-close">
+          <i className="fa fa-close"></i>
+        </div>
+        <div className="canvas-search search-switch">
+          <i className="fa fa-search"></i>
+        </div>
+        <nav className="canvas-menu mobile-menu">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/Sobre">Sobre Nós</Link>
+            </li>
+            <li>
+              <Link to="/Details">Aulas</Link>
+            </li>
+            <li>
+              <Link to="/Servicos">Servicos</Link>
+            </li>
+            <li>
+              <Link to="/NossoTime">Nosso Time</Link>
+            </li>
+            <li>
+              <Link to="/Details">Paginas</Link>
+              <ul className="dropdow">
+                <li>
+                  <Link to="/Sobre">Sobre Nós</Link>
+                </li>
+                <li>
+                  <Link to="/Horarios">Horarios</Link>
+                </li>
+                <li>
+                  <Link to="/NossoTime">Nosso Time</Link>
+                </li>
+                <li>
+                  <Link to="/Galeria">Galeria</Link>
+                </li>
+                <li>
+                  <Link to="/NossoBlog">Nosso Blog</Link>
+                </li>
+                <li>
+                  <Link to="/404">404</Link>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <Link to="/Contato">Contato</Link>
+            </li>
+          </ul>
+        </nav>
+        <div id="mobile-menu-wrap">
+          <div className="slicknav_menu">
+            <nav
+              className="slicknav_nav slicknav_hidden"
+              aria-hidden="true"
+              role="menu"
+            >
+              <ul>
+                <li>
+                  <Link to="/">Inicio</Link>
+                </li>
+                <li>
+                  <Link to="/Sobre">Sobre Nós</Link>
+                </li>
+                <li>
+                  <Link to="/Details">Aulas</Link>
+                </li>
+                <li>
+                  <Link to="/Servicos">Servicos</Link>
+                </li>
+                <li>
+                  <Link to="/NossoTime">Nosso Time</Link>
+                </li>
+                <li>
+                  <Link to="/Horarios">Horarios</Link>
+                </li>
+                <li>
+                  <Link to="/Galeria">Galeria</Link>
+                </li>
+                <li>
+                  <Link to="/NossoBlog">NossoBlog</Link>
+                </li>
+                <li>
+                  <Link to="/404">404</Link>
+                </li>
+                <li>
+                  <Link to="/Contato">Contato</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+        <div id="mobile-menu-wrap"></div>
+        <div className="canvas-social">
+          <a href="#">
+            <i className="fa fa-facebook"></i>
+          </a>
+          <a href="#">
+            <i className="fa fa-twitter"></i>
+          </a>
+          <a href="#">
+            <i className="fa fa-youtube-play"></i>
+          </a>
+          <a href="#">
+            <i className="fa fa-instagram"></i>
+          </a>
+        </div>
+      </div>
+      {/* Offcanvas Menu Section End */}
 
       {/* Header Section Begin */}
       <header className="header-section">
@@ -538,7 +653,138 @@ function Details() {
       </section>
       {/* Class Timetable Section End  */}
 
-      <Footer />
+      {/* Get In Touch Section Begin  */}
+      <div className="gettouch-section">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-4">
+              <div className="gt-text">
+                <i className="fa fa-map-marker"></i>
+                <p>
+                  Av. Santo Antônio
+                  <br />
+                  Capuchinhos, Feira de Santana - BA,
+                </p>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="gt-text">
+                <i className="fa fa-mobile"></i>
+                <p>(75) 9702-3456</p>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="gt-text email">
+                <i className="fa fa-envelope"></i>
+                <p>EvolucaoSports@gmail.com</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* Get In Touch Section End  */}
+
+      {/* Footer Section Begin  */}
+      <section className="footer-section">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-4">
+              <div className="fs-about">
+                <div className="fa-logo">
+                  <a href="#">
+                    <img src={Logo} alt="" />
+                  </a>
+                </div>
+                <p>
+                  A Evolução Sports é muito mais do que apenas uma academia
+                  comum; é um centro de transformação física e mental. Com uma
+                  ampla gama de modalidades e serviços disponíveis
+                </p>
+                <div className="fa-social">
+                  <a href="#">
+                    <i className="fa fa-facebook"></i>
+                  </a>
+                  <a href="#">
+                    <i className="fa fa-twitter"></i>
+                  </a>
+                  <a href="#">
+                    <i className="fa fa-youtube-play"></i>
+                  </a>
+                  <a
+                    href="https://www.instagram.com/evolucao.sports_/"
+                    target="_blank"
+                  >
+                    <i className="fa fa-instagram"></i>
+                  </a>
+                  <a href="#">
+                    <i className="fa  fa-envelope-o"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-4 col-md-6">
+              <div className="fs-widget">
+                <h4>Dicas e Guias</h4>
+                <div className="fw-recent">
+                  <h6>
+                    <a href="#">
+                      A aptidão física pode ajudar a prevenir a depressão e a
+                      ansiedade
+                    </a>
+                  </h6>
+                  <ul>
+                    <li>3 minutos de leitura</li>
+                    <li>20 Comentários</li>
+                  </ul>
+                </div>
+                <div className="fw-recent">
+                  <h6>
+                    <a href="#">
+                      Jiu Jitsu: A arte marcial suprema para eliminar a gordura
+                      abdominal e tonificar seu corpo enquanto domina
+                      habilidades de autodefesa.
+                    </a>
+                  </h6>
+                  <ul>
+                    <li>3 minutos de leitura</li>
+                    <li>20 Comentários</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-lg-12 text-center">
+              <div className="copyright-text">
+                <p>
+                  Copyright &copy;
+                  <script>document.write(new Date().getFullYear());</script> All
+                  rights reserved | This is made with
+                  <i className="fa fa-heart" aria-hidden="true"></i> Isis
+                  <a href="https://colorlib.com" target="_blank"></a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* Footer Section End  */}
+
+      {/* Search model Begin  */}
+      <div className="search-model">
+        <div className="h-100 d-flex align-items-center justify-content-center">
+          <div className="search-close-switch">+</div>
+          <form className="search-model-form">
+            <input
+              type="text"
+              id="search-input"
+              placeholder="Search here....."
+            />
+          </form>
+        </div>
+      </div>
+      {/* Search model end  */}
     </div>
   );
 }
--- src/menu/Servicos.js
+++ src/menu/Servicos.js
@@ -12,14 +12,11 @@ import "../css/magnific-popup.css";
 import "../css/slicknav.min.css";
 import "../css/style.css";
 import { Link } from "react-router-dom";
-import Cabecalho from "../components/cabecalho";
 
 import Services1 from "../img/services/services-1.jpg";
 import Services2 from "../img/services/services-2.jpg";
 import Services3 from "../img/services/services-3.jpg";
 import Services4 from "../img/services/services-4.jpg";
-import Footer from "../components/footer";
-import Preco from "../components/preco";
 
 function Servicos() {
   useEffect(() => {
@@ -35,7 +32,125 @@ function Servicos() {
   }, []);
   return (
     <div className="App">
-      <Cabecalho />
+      {/* Page Preloder */}
+      <div className="preloder">
+        <div className="loader"></div>
+      </div>
+
+      {/* Offcanvas Menu Section Begin */}
+      <div className="offcanvas-menu-overlay active"></div>
+      <div className="offcanvas-menu-wrapper">
+        <div className="canvas-close">
+          <i className="fa fa-close"></i>
+        </div>
+        <div className="canvas-search search-switch">
+          <i className="fa fa-search"></i>
+        </div>
+        <nav className="canvas-menu mobile-menu">
+          <ul>
+            <li>
+              <Link to="/">Inicio</Link>
+            </li>
+            <li>
+              <Link to="/Sobre">Sobre Nós</Link>
+            </li>
+            <li>
+              <Link to="/Details">Aulas</Link>
+            </li>
+            <li>
+              <Link to="/Servicos">Servicos</Link>
+            </li>
+            <li>
+              <Link to="/NossoTime">Nosso Time</Link>
+            </li>
+            <li>
+              <Link to="/Servicos">Paginas</Link>
+              <ul className="dropdow">
+                <li>
+                  <Link to="/Sobre">Sobre Nós</Link>
+                </li>
+                <li>
+                  <Link to="/Horarios">Horarios</Link>
+                </li>
+
+                <li>
+                  <Link to="/NossoTime">Nosso Time</Link>
+                </li>
+                <li>
+                  <Link to="/Galeria">Galeria</Link>
+                </li>
+                <li>
+                  <Link to="/NossoBlog">Nosso Blog</Link>
+                </li>
+                <li>
+                  <Link to="/404">404</Link>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <Link to="/Contato">Contato</Link>
+            </li>
+          </ul>
+        </nav>
+        <div id="mobile-menu-wrap">
+          <div className="slicknav_menu">
+            <nav
+              className="slicknav_nav slicknav_hidden"
+              aria-hidden="true"
+              role="menu"
+            >
+              <ul>
+                <li>
+                  <Link to="/">Inicio</Link>
+                </li>
+                <li>
+                  <Link to="/Sobre">Sobre Nós</Link>
+                </li>
+                <li>
+                  <Link to="/Details">Aulas</Link>
+                </li>
+                <li>
+                  <Link to="/Servicos">Servicos</Link>
+                </li>
+                <li>
+                  <Link to="/NossoTime">Nosso Time</Link>
+                </li>
+                <li>
+                  <Link to="/Horarios">Horarios</Link>
+                </li>
+
+                <li>
+                  <Link to="/Galeria">Galeria</Link>
+                </li>
+                <li>
+                  <Link to="/NossoBlog">NossoBlog</Link>
+                </li>
+                <li>
+                  <Link to="/404">404</Link>
+                </li>
+                <li>
+                  <Link to="/Contato">Contato</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+        <div className="canvas-social">
+          <a href="#">
+            <i className="fa fa-facebook"></i>
+          </a>
+          <a href="#">
+            <i className="fa fa-twitter"></i>
+          </a>
+          <a href="#">
+            <i className="fa fa-youtube-play"></i>
+          </a>
+          <a href="#">
+            <i className="fa fa-instagram"></i>
+          </a>
+        </div>
+      </div>
+      {/* Offcanvas Menu Section End */}
 
       {/* Header Section Begin */}
       <header className="header-section">
@@ -245,7 +360,6 @@ function Servicos() {
                 <a
                   href="https://www.youtube.com/watch?v=MPwc95pA_IE"
                   className="play-btn video-popup"
-                  target="_blank"
                 >
                   <i className="fa fa-caret-right"></i>
                 </a>
@@ -256,9 +370,224 @@ function Servicos() {
       </section>
       {/* Banner Section End  */}
 
-      <Preco />
+      {/* Pricing Section Begin  */}
+      <section className="pricing-section spad">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">
+              <div className="section-title">
+                <span>Nossos Planos</span>
+                <h2>Escolha seu plano de preços</h2>
+              </div>
+            </div>
+          </div>
+          <div className="row justify-content-center">
+            <div className="col-lg-4 col-md-8">
+              <div className="ps-item">
+                <h3>Aula presencial</h3>
+                <div className="pi-price">
+                  <h2>R$ XX.XX</h2>
+                  <span>classe única</span>
+                </div>
+                <ul>
+                  <li>Volta gratuita</li>
+                  <li>Equipamentos ilimitados</li>
+                  <li>Treinador pessoal</li>
+                  <li>Aulas para perder peso</li>
+                  <li>Sem restrição de tempo</li>
+                </ul>
+                <a
+                  href="https://api.whatsapp.com/send?1=pt_BR&phone=5575981864590"
+                  target="_blank"
+                  className="primary-btn pricing-btn"
+                >
+                  Inscreva-se agora
+                </a>
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-8">
+              <div className="ps-item">
+                <h3>12 meses ilimitados</h3>
+                <div className="pi-price">
+                  <h2>R$ XX.XX</h2>
+                  <span>Classe Única</span>
+                </div>
+                <ul>
+                  <li>Volta gratuita</li>
+                  <li>Equipamentos ilimitados</li>
+                  <li>Treinador pessoal</li>
+                  <li>Aulas para perder peso</li>
+                  <li>Sem restrição de tempo</li>
+                </ul>
+                <a
+                  href="https://api.whatsapp.com/send?1=pt_BR&phone=5575981864590"
+                  target="_blank"
+                  className="primary-btn pricing-btn"
+                >
+                  Inscreva-se agora
+                </a>
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-8">
+              <div className="ps-item">
+                <h3>6 meses ilimitados</h3>
+                <div className="pi-price">
+                  <h2>R$ XX.XX</h2>
+                  <span>Classe única</span>
+                </div>
+                <ul>
+                  <li>Volta gratuita</li>
+                  <li>Equipamentos ilimitados</li>
+                  <li>Treinador pessoal</li>
+                  <li>Aulas para perder peso</li>
+                  <li>Sem restrição de tempo</li>
+                </ul>
+                <a
+                  href="https://api.whatsapp.com/send?1=pt_BR&phone=5575981864590"
+                  target="_blank"
+                  className="primary-btn pricing-btn"
+                >
+                  Inscreva-se agora
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* Pricing Section End  */}
 
-      <Footer />
+      {/* Get In Touch Section Begin  */}
+      <div className="gettouch-section">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-4">
+              <div className="gt-text">
+                <i className="fa fa-map-marker"></i>
+                <p>
+                  Av. Santo Antônio
+                  <br />
+                  Capuchinhos, Feira de Santana - BA,
+                </p>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="gt-text">
+                <i className="fa fa-mobile"></i>
+                <p>(75) 9702-3456</p>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="gt-text email">
+                <i className="fa fa-envelope"></i>
+                <p>EvolucaoSports@gmail.com</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* Get In Touch Section End  */}
+
+      {/* Footer Section Begin  */}
+      <section className="footer-section">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-4">
+              <div className="fs-about">
+                <div className="fa-logo">
+                  <a href="#">
+                    <img src={Logo} alt="" />
+                  </a>
+                </div>
+                <p>
+                  A Evolução Sports é muito mais do que apenas uma academia
+                  comum; é um centro de transformação física e mental. Com uma
+                  ampla gama de modalidades e serviços disponíveis
+                </p>
+                <div className="fa-social">
+                  <a href="#">
+                    <i className="fa fa-facebook"></i>
+                  </a>
+                  <a href="#">
+                    <i className="fa fa-twitter"></i>
+                  </a>
+                  <a href="#">
+                    <i className="fa fa-youtube-play"></i>
+                  </a>
+                  <a
+                    href="https://www.instagram.com/evolucao.sports_/"
+                    target="_blank"
+                  >
+                    <i className="fa fa-instagram"></i>
+                  </a>
+                  <a href="#">
+                    <i className="fa  fa-envelope-o"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-lg-4 col-md-6">
+              <div className="fs-widget">
+                <h4>Dicas e Guias</h4>
+                <div className="fw-recent">
+                  <h6>
+                    <a href="#">
+                      A aptidão física pode ajudar a prevenir a depressão e a
+                      ansiedade
+                    </a>
+                  </h6>
+                  <ul>
+                    <li>3 minutos de leitura</li>
+                    <li>20 Comentários</li>
+                  </ul>
+                </div>
+                <div className="fw-recent">
+                  <h6>
+                    <a href="#">
+                      Jiu Jitsu: A arte marcial suprema para eliminar a gordura
+                      abdominal e tonificar seu corpo enquanto domina
+                      habilidades de autodefesa.
+                    </a>
+                  </h6>
+                  <ul>
+                    <li>3 minutos de leitura</li>
+                    <li>20 Comentários</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-lg-12 text-center">
+              <div className="copyright-text">
+                <p>
+                  Copyright &copy;
+                  <script>document.write(new Date().getFullYear());</script> All
+                  rights reserved | This is made with
+                  <i className="fa fa-heart" aria-hidden="true"></i> Isis
+                  <a href="https://colorlib.com" target="_blank"></a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* Footer Section End  */}
+
+      {/* Search model Begin  */}
+      <div className="search-model">
+        <div className="h-100 d-flex align-items-center justify-content-center">
+          <div className="search-close-switch">+</div>
+          <form className="search-model-form">
+            <input
+              type="text"
+              id="search-input"
+              placeholder="Search here....."
+            />
+          </form>
+        </div>
+      </div>
+      {/* Search model end  */}
     </div>
   );
 }
